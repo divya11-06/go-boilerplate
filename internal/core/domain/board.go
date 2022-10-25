f513ccc307e3bb0c1fef5a40e677edacfb6ded21
accio112 <divyaatnet2015@gmail.com>
2022-10-25 14:54:31 +0530

    added core code

--- internal/core/domain/board.go
+++ internal/core/domain/board.go
@@ -1,1 +1,9 @@
 package domain
+
+type GamesRepository interface {
+
+}
+
+type GamesService interface {
+
+}
--- internal/core/services/service.go
+++ internal/core/services/service.go
@@ -1,1 +1,10 @@
 package services
+
+type service struct {}
+
+func New() *service {
+	return &service{}
+}
+
+func (srv *service) Get(id string) () {
+}
