4e06be99e84f55d5b622bb42926f8359f4b233eb
accio112 <divyaatnet2015@gmail.com>
2022-10-25 14:50:54 +0530

    added types

--- internal/core/domain/game.go
+++ internal/core/domain/game.go
@@ -1,1 +1,16 @@
 package domain
+
+type Game struct {
+	ID            string        `json:"id"`
+	Name          string        `json:"name"`
+	State         string        `json:"state"`
+	BoardSettings BoardSettings `json:"board_settings"`
+	Board         Board         `json:"board"`
+}
+
+type BoardSettings struct {
+	Size  uint `json:"size"`
+	Bombs uint `json:"bombs"`
+}
+
+type Board [][]string
